e876dd24d2144d5c8b78d4796159c52351b01578
digimarc-corp <dl-github-devops@digimarc.com>
2022-05-26 08:46:18 -0700

    Automated commit

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "DMSDK",
+    platforms: [
+        .macOS(.v10_13), .iOS(.v11), .tvOS(.v12), .watchOS(.v4)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "DMSDK",
+            targets: ["DMSDK"])
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(
+            name: "DMSDK",
+            url: "https://dmrc.io/packages/xcframework/DMSDK/3.5.1-beta1/DMSDK-Package.zip",
+            checksum: "cb736316cb316276df0231bc28854a20fafc6a38e1675a3d7ec9e8906316aa67"
+        )
+    ]
+)
